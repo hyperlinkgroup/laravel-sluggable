4b247ae807ac97dfcc963373333bc1dbff717c09
Katalam <bruno.goerss@hyperlink.eu>
2023-02-26 22:59:38 +0100

    Adding latest slug with ->slug and ->slugHistory

--- src/Traits/Sluggable.php
+++ src/Traits/Sluggable.php
@@ -2,11 +2,33 @@
 
 namespace Hyperlink\Sluggable\Traits;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
 
 trait Sluggable {
     public function slugs(): MorphMany
     {
         return $this->morphMany(config('sluggable.model'), 'sluggable');
+    }
+
+    public function latestSlug(): MorphOne
+    {
+        return $this->morphOne(config('sluggable.model'), 'sluggable')->latestOfMany();
+    }
+
+    public function slug(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => $this->latestSlug->slug,
+            set: fn ($value) => $this->slugs()->create(['slug' => $value])
+        );
+    }
+
+    public function slugHistory(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => $this->slugs()->latest()->get()->pluck('slug')->toArray(),
+        );
     }
 }
--- tests/SchemaTest.php
+++ tests/SchemaTest.php
@@ -16,3 +16,48 @@ it('can link slugs with the trait', function () {
 
     expect($post->slugs()->count())->toBe(1);
 });
+
+it('always returns the latest slug', function () {
+    $post = Post::create([
+        'title' => 'Test Title',
+    ]);
+
+    Slug::create([
+        config('sluggable.column') => 'test-slug',
+        'sluggable_id' => $post->id,
+        'sluggable_type' => Post::class,
+    ]);
+
+    Slug::create([
+        config('sluggable.column') => 'test-slug-2',
+        'sluggable_id' => $post->id,
+        'sluggable_type' => Post::class,
+    ]);
+
+    expect($post->slugs()->count())->toBe(2);
+    expect($post->slug)->toBe('test-slug-2');
+});
+
+it('can show the slug history', function () {
+    $post = Post::create([
+        'title' => 'Test Title',
+    ]);
+
+    Slug::create([
+        config('sluggable.column') => 'test-slug',
+        'sluggable_id' => $post->id,
+        'sluggable_type' => Post::class,
+    ]);
+
+    Slug::create([
+        config('sluggable.column') => 'test-slug-2',
+        'sluggable_id' => $post->id,
+        'sluggable_type' => Post::class,
+    ]);
+
+    expect($post->slugs()->count())->toBe(2);
+    expect($post->slugHistory)->toBe([
+        'test-slug',
+        'test-slug-2',
+    ]);
+});
